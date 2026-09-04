cbec10b886485c0a47088d315bb7bb1f277913af
Tausif Alam <123tausifalam123@gmail.com>
2026-09-04 20:47:11 +0530

    initial Commit

---
+++ src/main/java/tech/tausif/CargoZen/CargoZenApplication.java
@@ -0,0 +1,13 @@
+package tech.tausif.CargoZen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/tausif/CargoZen/CargoZenApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tausif.CargoZen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
