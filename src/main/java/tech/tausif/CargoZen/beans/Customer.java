9ee7ca35fd3d534dbdd0805887d3131a234364d9
Tausif Alam <123tausifalam123@gmail.com>
2026-09-10 22:32:12 +0530

    add customer bean

---
+++ src/main/java/tech/tausif/CargoZen/beans/Customer.java
@@ -0,0 +1,11 @@
+package tech.tausif.CargoZen.beans;
+
+public class Customer {
+
+
+    private String email;
+    private String username;
+    private String name;
+    private String phone;
+    private String password;
+}
