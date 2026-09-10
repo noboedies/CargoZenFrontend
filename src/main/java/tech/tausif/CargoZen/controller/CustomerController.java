21cd983d0af6bcd059d7ac866e65ac89ce9b134e
Tausif Alam <123tausifalam123@gmail.com>
2026-09-10 22:58:55 +0530

    update

--- src/main/java/tech/tausif/CargoZen/controller/CustomerController.java
+++ src/main/java/tech/tausif/CargoZen/controller/CustomerController.java
@@ -4,6 +4,7 @@ package tech.tausif.CargoZen.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +30,10 @@ public class CustomerController {
             ra.addFlashAttribute("msg", "Customer Email Already Exist");
             return "redirect:/register";
         }
+    }
+
+    @GetMapping("/customer-dashboard")
+    public String customerDashboard(){
+        return "customer-dashboard";
     }
 }
